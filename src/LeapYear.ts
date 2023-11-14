6568c83d7a8f2f5b9787d6217608c7c91c3503a2
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-11-14 11:59:40 +0000

    refactor: use common logic for the years divisible by 100 and 400

--- src/LeapYear.ts
+++ src/LeapYear.ts
@@ -7,9 +7,8 @@ const isValidYear = (year: number) => {
 const isLeapYear = (year: number) => {
   isValidYear(year);
 
-  if (year === 1700 || year === 1800) return false;
-
-  if (year % 4 === 0) return true;
+  if (year % 400 === 0) return true;
+  if (year % 4 === 0 && year % 100 !== 0) return true;
 
   return false;
 };
